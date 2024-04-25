e6f0bf2e9249bfac03e799d856dbdb71221a012d
The-GreenCraft <th3greencraft@gmail.com>
2024-04-25 10:16:01 +0200

    Update application layout and JS to handle notice flash message
    * Update application.css to adjust the position and size of notice div
    * Remove outdated window.onload function from application.js
    * Add a click event listener to each flash

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,13 +2,3 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-
-window.onload = () => {
-    let flash = document.getElementsByClassName("flash");
-    console.log(flash);
-    flash.forEach(msg => {
-        msg.onclick = (evt) => {
-            console.log("clicked");
-        }  
-    });
-}
