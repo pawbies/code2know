90b1871706c19adcda67570c15f23d3d79133e52
The-GreenCraft <th3greencraft@gmail.com>
2024-05-02 13:54:14 +0200

    yay

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,9 +3,11 @@ class Question < ApplicationRecord
   has_many :answers
   has_and_belongs_to_many :categories
 
+  validates :heading, presence: true
   validates :heading, length: { minimum: 0 }
   validates :heading, length: { maximum: 25 }
 
+  validates :text, presence: true
   validates :text, length: { minimum: 0 }
   validates :text, length: { maximum: 1000 }
 end
