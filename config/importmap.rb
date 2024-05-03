1a34589d57be6633b3926669d7599b132ef3e63d
CHoneyfox <75548757+CHoneyfox@users.noreply.github.com>
2024-05-03 15:24:40 +0200

    things happened

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "markdown_converter", preload: false
 pin "preview", preload: false
 pin "showdown" # @2.1.0
 pin "highlight.js", to: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/es/highlight.min.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
