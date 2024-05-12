891053a6942e8f8aaf1216221902c2b5f250c418
CHoneyfox <75548757+CHoneyfox@users.noreply.github.com>
2024-05-12 18:02:05 +0200

    forgot

--- db/migrate/20240422062904_create_answers.rb
+++ db/migrate/20240422062904_create_answers.rb
@@ -2,8 +2,8 @@ class CreateAnswers < ActiveRecord::Migration[7.1]
   def change
     create_table :answers do |t|
       t.text :text
-      t.references :user, null: false, foreign_key: true, foreign_key: { on_delete: :cascade }
-      t.references :question, null: true, foreign_key: true, foreign_key: { on_delete: :cascade }
+      t.references :user, null: false, foreign_key: true
+      t.references :question, null: true, foreign_key: true
       t.references :answer, null: true, foreign_key: true
 
       t.timestamps
