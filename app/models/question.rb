fe2d74f2baae0428bde215fa502e2648fb35b948
CHoneyfox <75548757+CHoneyfox@users.noreply.github.com>
2024-05-13 19:24:14 +0200

    fixed reports

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,10 @@
 class Question < ApplicationRecord
   belongs_to :user
-  has_many :answers
-  has_many :reports
+  has_many :answers, dependent: :delete_all
+  has_many :reports, dependent: :delete_all
   has_and_belongs_to_many :categories
+
+  before_destroy :remove_category_associatations
 
   validates :heading, presence: true
   validates :heading, length: { minimum: 0 }
@@ -11,4 +13,10 @@ class Question < ApplicationRecord
   validates :text, presence: true
   validates :text, length: { minimum: 0 }
   validates :text, length: { maximum: 3000 }
+
+  private
+
+  def remove_category_associatations
+    categories.clear
+  end
 end
